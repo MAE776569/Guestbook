b01e98906938f67bcf3b5e0cb6209a9f5aa1cf35
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-12 15:23:09 +0200

    Created PageNotFound component

---
+++ front-end/src/components/PageNotFound.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function PageNotFound() {
+  return (
+    <div className="not-found">
+      <div>
+        <h1 className="not-found__title">404</h1>
+        <div className="not-found-subheader">
+          <h2 className="not-found__subtitle">This page could not be found</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PageNotFound
