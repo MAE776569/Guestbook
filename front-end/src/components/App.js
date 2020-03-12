d38b4ee25dfa4bf4fcd5faa07193b395df71ab24
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-12 15:27:02 +0200

    Added routes for navigation

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react"
-import NavBar from "./NavBar"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Login from "./Login"
+import SignUp from "./SignUp"
+import PageNotFound from "./PageNotFound"
 
 function App() {
   return (
-  <>
-    <NavBar />
-    <Login />
-  </>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/Signup" component={SignUp} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </BrowserRouter>
   )
 }
 
