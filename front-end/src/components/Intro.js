74f501020a415b45871fa2926d1a051d308adc97
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-12 14:10:18 +0200

    Created Intro component

---
+++ front-end/src/components/Intro.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Intro() {
+  return (
+    <div className="col-12 col-md-8 col-lg-5 m-auto">
+      <h1>Guestbook</h1>
+      <p className="lead">
+        Send messages to other members and reply to messages
+      </p>
+    </div>
+  )
+}
+
+export default Intro
