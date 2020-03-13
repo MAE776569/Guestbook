739af36c18a74f6cbcc4026206836b952b710168
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-13 20:55:09 +0200

    Created signup route

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const authenticateUser = require("../middlewares/authentication")
 const Session = require("../models/session")
+const User = require("../models/user")
 
 router.post("/login", (req, res) => {
   req.checkBody("username", "Username is required").notEmpty()
@@ -21,6 +22,33 @@ router.post("/login", (req, res) => {
         return res.json({ sessionID: session._id })
       })
     }
+  })
+})
+
+router.post("/signup", (req, res) => {
+  req.checkBody("name", "Name is required").notEmpty()
+  req.checkBody("name", "Name consists only of letters").isAlpha()
+  req.checkBody("username", "Username is required").notEmpty()
+  req.checkBody("password", "Password is required").notEmpty()
+  req
+    .checkBody("username", "Username consists only of alpha numerics")
+    .isAlphanumeric()
+  req.checkBody("c-password", "Passwords must match").equals("password")
+
+  const errors = req.validationErrors()
+  if (errors) return res.json({ errors })
+
+  const { name, username, password } = req.body
+  User.create({
+    name,
+    username,
+    password 
+  }, (error, user) => {
+    if(error) return res.json({ error })
+    Session.create({ user: user._id }, (error, session) => {
+      if (error) throw error
+      return res.json({ sessionID: session._id })
+    })
   })
 })
 
