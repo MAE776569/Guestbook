15e651eacc607b561a661e61192b7ebe8739f808
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-14 18:58:22 +0200

    Convert form to controlled component

--- front-end/src/components/SearchUsers.js
+++ front-end/src/components/SearchUsers.js
@@ -1,19 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 
 // Component to search for users
 function SearchUsers() {
+  const [searchQuery, setSearchQuery] = useState("")
+
   return (
     <div className="d-flex flex-nowrap search-users">
-      <div className="input-group">
-        <input
-          type="text"
-          className="form-control search-users__input"
-          placeholder="Search Users"
-        />
-         <div className="input-group-append">
-          <span className="input-group-text">@</span>
+      <form className="w-100">
+        <div className="input-group">
+          <input
+            type="text"
+            className="form-control search-users__input"
+            placeholder="Enter name or username"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <div className="input-group-append">
+            <button type="submit" className="btn btn-outline-primary">
+              Search
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
