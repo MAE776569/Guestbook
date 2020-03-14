9c0ba0a842f49a89aa07cce08b45e44604c4ef15
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-14 18:01:21 +0200

    Created user item component

---
+++ front-end/src/components/UserItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function UserItem(){
+  return (
+    <li className="list-group-item list-group-item-action">
+      <h6 className="user-item__name">Name</h6>
+      <p className="user-item__username">uesrname</p>
+    </li>
+  )
+}
+
+export default UserItem
