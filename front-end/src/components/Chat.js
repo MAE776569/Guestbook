ee0f73019d74f1b6ff58b0c4d90e20cba8b4809b
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-14 18:37:01 +0200

    Fetch users from backend

--- front-end/src/components/Chat.js
+++ front-end/src/components/Chat.js
@@ -1,33 +1,54 @@
-import React from "react"
+import React, { Component } from "react"
 import NavBar from "./NavBar"
 import SearchUsers from "./SearchUsers"
 import UsersList from "./UsersList"
 import Messages from "./Messages"
 import { connectStore } from "../store"
 import { withRouter, Redirect } from "react-router-dom"
+import { getUsers } from "../utils/API"
 
 /* The main page consists of two section:
 messages to view and CRUD messages
 users to search and select users to their messages */
-function Chat(props) {
-  if (!props.loggedUser) return <Redirect to="/login" />
+class Chat extends Component {
+  state = {
+    users: []
+  }
 
-  return (
-    <>
-      <NavBar />
-      <div className="container pt-1 chat">
-        <div className="row px-3">
-          <div className="col-12 col-md-6 col-lg-8 order-1 ordre-md-0 messages-section">
-            <Messages />
-          </div>
-          <div className="col-12 col-md-6 col-lg-4 order-0 order-md-1 users-section">
-            <SearchUsers />
-            <UsersList />
+  componentDidMount(){
+    getUsers()
+      .then((res) => {
+        if (!res.error)
+          this.setState({ users: res.users })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    const { loggedUser } = this.props
+    if (!loggedUser) return <Redirect to="/login" />
+
+    let { users } = this.state
+    users = users.filter((user) => user.id !== loggedUser.id)
+    
+    return (
+      <>
+        <NavBar />
+        <div className="container pt-1 chat">
+          <div className="row px-3">
+            <div className="col-12 col-md-6 col-lg-8 order-1 ordre-md-0 messages-section">
+              <Messages />
+            </div>
+            <div className="col-12 col-md-6 col-lg-4 order-0 order-md-1 users-section">
+              <SearchUsers />
+              <UsersList users={users} />
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
-
 export default withRouter(connectStore(Chat))
