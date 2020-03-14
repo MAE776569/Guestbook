0d7804ec3fa44453de4ccffa93380fdf305b6b00
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-14 20:10:02 +0200

    Style empty user list

--- front-end/src/components/UsersList.js
+++ front-end/src/components/UsersList.js
@@ -3,7 +3,12 @@ import UserItem from "./UserItem"
 
 // Users list shows the available users
 function UsersList({ users, activeUserID, setActiveUser }) {
-  if (users.length === 0) return <div>No available users!</div>
+  if (users.length === 0)
+    return (
+      <div className="empty-user-list d-flex justify-content-center align-items-center">
+        No available users!
+      </div>
+    )
 
   return (
     <ul className="list-group">
