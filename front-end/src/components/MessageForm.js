8beaf25b6fd50a3158b1b8deb05d5c27f835583b
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-15 12:51:27 +0200

    Control message form state

--- front-end/src/components/MessageForm.js
+++ front-end/src/components/MessageForm.js
@@ -1,14 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 
-function MessageForm() {
+// Controlled component for message input
+function MessageForm({ sendMessage }) {
+  const [message, setMessage] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    sendMessage(message)
+  }
+
   return (
     <div className="d-flex flex-nowrap message-form">
-      <form className="w-100">
+      <form className="w-100" onSubmit={handleSubmit}>
         <div className="input-group">
           <textarea
             className="message-form__input"
             placeholder="Enter Your Message"
             spellCheck={true}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
           <div className="input-group-append">
             <button type="submit" className="btn btn-outline-primary">
