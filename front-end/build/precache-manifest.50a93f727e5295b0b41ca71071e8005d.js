fb2b5521da8432da4def9f89758967ae4ab78731
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2020-03-15 21:41:41 +0200

    Build files

---
+++ front-end/build/precache-manifest.50a93f727e5295b0b41ca71071e8005d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f26d30c11fe3841bdfef",
+    "url": "/static/js/main.f26d30c1.chunk.js"
+  },
+  {
+    "revision": "8e6cb11cbc358cf190ba",
+    "url": "/static/js/1.8e6cb11c.chunk.js"
+  },
+  {
+    "revision": "f26d30c11fe3841bdfef",
+    "url": "/static/css/main.bd478b06.chunk.css"
+  },
+  {
+    "revision": "06b90d23fb8a87f77bd471f447ba3f3c",
+    "url": "/index.html"
+  }
+];
